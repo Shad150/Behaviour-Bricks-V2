fa5894ecca1532d7989217b9bc52d6be71a06313
Shad150 <cgpablo150@gmail.com>
2020-06-02 23:05:38 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerIsChased.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+using Pada1.BBCore;           // Code attributes
+using Pada1.BBCore.Framework; // ConditionBase
+
+[Condition("MyConditions/IsChased")]
+[Help("Checks whether it is night. It searches for the first light labeled with " +
+      "the 'MainLight' tag, and looks for its DayNightCycle script, returning the" +
+      "informed state. If no light is found, false is returned.")]
+public class PlayerIsChased : ConditionBase
+{
+    public override bool Check()
+    {
+        return GlobalBlackboard.playerChased;
+    }
+}
---
+++ Assets/Scripts/SetEnemyChasing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pada1.BBCore;
+using Pada1.BBCore.Tasks;
+using Pada1.BBCore.Framework;
+
+
+[Action("MyActions/SetEnemyChasing")]
+public class SetEnemyChasing : BasePrimitiveAction
+{
+    [InParam("value")]
+    public bool value;
+
+    public override TaskStatus OnUpdate()
+    {
+        GlobalBlackboard.playerChased = value;
+
+        return TaskStatus.COMPLETED; 
+    }
+}
